d16eaf053575ca9c39c400378cbc751d82c85f94
John Murray <johnmurrayvi@gmail.com>
2012-04-23 23:07:06 -0400

    chapter 7-8 stuff and some mount/umount/chroot scripts

--- LFS-chroot-notools.sh
+++ LFS-chroot-notools.sh
@@ -2,5 +2,8 @@
 
 LFS=/mnt/lfs
 
-sudo chroot "$LFS" /usr/bin/env -i HOME=/root TERM="$TERM" PS1='\u:\w\$ ' PATH=/bin:/usr/bin:/sbin:/usr/sbin /bin/bash --login +h
+sudo chroot "$LFS" /usr/bin/env -i \
+	HOME=/root TERM="$TERM" PS1='\u:\w\$ ' \
+	PATH=/bin:/usr/bin:/sbin:/usr/sbin \
+	/bin/bash --login
 
--- chap6-pt2.sh
+++ chap6-pt2.sh
@@ -118,14 +118,16 @@ rm -rf man-pages-3.35
 tar jxf glibc-2.14.1.tar.bz2 
 cd glibc-2.14.1
 DL=$(readelf -l /bin/sh | sed -n 's@.*interpret.*/tools\(.*\)]$@\1@p')
-sed -i "s|libs -o|libs -L/usr/lib -Wl,-dynamic-linker=$DL -o|"         scripts/test-installation.pl
+sed -i "s|libs -o|libs -L/usr/lib -Wl,-dynamic-linker=$DL -o|" \
+	scripts/test-installation.pl
 unset DL
 sed -i -e 's/"db1"/& \&\& $name ne "nss_test1"/' scripts/test-installation.pl
 sed -i 's|@BASH@|/bin/bash|' elf/ldd.bash.in
 patch -Np1 -i ../glibc-2.14.1-fixes-1.patch
 patch -Np1 -i ../glibc-2.14.1-sort-1.patch 
 patch -Np1 -i ../glibc-2.14.1-gcc_fix-1.patch
-sed -i '195,213 s/PRIVATE_FUTEX/FUTEX_CLOCK_REALTIME/' nptl/sysdeps/unix/sysv/linux/x86_64/pthread_rwlock_timed{rd,wr}lock.S
+sed -i '195,213 s/PRIVATE_FUTEX/FUTEX_CLOCK_REALTIME/' \
+	nptl/sysdeps/unix/sysv/linux/x86_64/pthread_rwlock_timed{rd,wr}lock.S
 mkdir -v ../glibc-build
 cd ../glibc-build/
 ../glibc-2.14.1/configure --prefix=/usr \
@@ -212,7 +214,10 @@ mv -v /tools/bin/{ld,ld-old}
 mv -v /tools/$(gcc -dumpmachine)/bin/{ld,ld-old}
 mv -v /tools/bin/{ld-new,ld}
 ln -sv /tools/bin/ld /tools/$(gcc -dumpmachine)/bin/ld
-gcc -dumpspecs | sed -e 's@/tools@@g' -e '/\*startfile_prefix_spec:/{n;s@.*@/usr/lib/ @}' -e '/\*cpp:/{n;s@$@ -isystem /usr/include@}' > `dirname $(gcc --print-libgcc-file-name)`/specs
+gcc -dumpspecs | sed -e 's@/tools@@g' \
+	-e '/\*startfile_prefix_spec:/{n;s@.*@/usr/lib/ @}' \
+	-e '/\*cpp:/{n;s@$@ -isystem /usr/include@}' \
+	> `dirname $(gcc --print-libgcc-file-name)`/specs
 dirname $(gcc --print-libgcc-file-name)
 cat /tools/lib/gcc/x86_64-unknown-linux-gnu/4.6.2/specs 
 echo 'main(){}' > dummy.c
@@ -355,7 +360,9 @@ esac
 sed -i 's@\./fixinc\.sh@-c true@' gcc/Makefile.in 
 mkdir ../gcc-build
 cd ../gcc-build/
-../gcc-4.6.2/configure --prefix=/usr --libexecdir=/usr/lib --enable-shared --enable-threads=posix --enable-__cxa_atexit --enable-clocale=gnu --enable-languages=c,c++ --disable-multilib --disable-bootstrap --with-system-zlib
+../gcc-4.6.2/configure --prefix=/usr --libexecdir=/usr/lib --enable-shared \
+	--enable-threads=posix --enable-__cxa_atexit --enable-clocale=gnu \
+	--enable-languages=c,c++ --disable-multilib --disable-bootstrap --with-system-zlib
 make
 ulimit -s 16384
 make -k check
@@ -428,7 +435,13 @@ make
 make install
 mv -v /usr/lib/libncursesw.so.5* /lib
 ln -sfv ../../lib/libncursesw.so.5 /usr/lib/libncursesw.so 
-for lib in ncurses form panel menu ; do     rm -vf /usr/lib/lib${lib}.so ;     echo "INPUT(-l${lib}w)" >/usr/lib/lib${lib}.so ;     ln -sfv lib${lib}w.a /usr/lib/lib${lib}.a ; done
+
+for lib in ncurses form panel menu ; do
+	rm -vf /usr/lib/lib${lib}.so
+	echo "INPUT(-l${lib}w)" >/usr/lib/lib${lib}.so
+	ln -sfv lib${lib}w.a /usr/lib/lib${lib}.a
+done
+
 ln -sfv libncurses++w.a /usr/lib/libncurses++w.a 
 rm -vf /usr/lib/libcursesw.so
 echo "INPUT(-lncursesw)" > /usr/lib/libcursesw.so
@@ -438,7 +451,8 @@ ln -sfv libncurses.a /usr/lib/libcurses.a
 mkdir -v /usr/share/doc/ncurses-5.9
 cp -v -R doc/* /usr/share/doc/ncurses-5.9
 make distclean
-./configure --prefix=/usr --with-shared --without-normal --without-debug --without-cxx-binding
+./configure --prefix=/usr --with-shared --without-normal \
+	--without-debug --without-cxx-binding
 make sources libs
 cp -av lib/lib*.so.5* /usr/lib
 cd ..
@@ -482,7 +496,10 @@ tar xf e2fsprogs-1.42.tar.gz
 cd e2fsprogs-1.42
 mkdir build
 cd build/
-PKG_CONFIG=/tools/bin/true LDFLAGS="-lblkid -luuid" ../configure --prefix=/usr --with-root-prefix=/ --enable-elf-shlibs --disable-libblkid --disable-libuuid --disable-uuidd --disable-fsck
+PKG_CONFIG=/tools/bin/true LDFLAGS="-lblkid -luuid" \
+	../configure --prefix=/usr --with-root-prefix=/ \
+	--enable-elf-shlibs --disable-libblkid \
+	--disable-libuuid --disable-uuidd --disable-fsck
 make
 make check
 make install
@@ -567,7 +584,6 @@ rm -rf bison-2.5
 
 tar xf procps-3.2.8.tar.gz 
 cd procps-3.2.8
-patch -Np1 ../procps-3.2.8-fix_HZ_errors-1.patch 
 patch -Np1 -i ../procps-3.2.8-fix_HZ_errors-1.patch 
 patch -Np1 -i ../procps-3.2.8-watch_unicode-1.patch 
 sed -i -e 's@\*/module.mk@proc/module.mk ps/module.mk@' Makefile 
@@ -601,11 +617,6 @@ cd readline-6.2
 sed -i '/MV.*old/d' Makefile.in 
 sed -i '/{OLDSUFF}/c:' support/shlib-install 
 patch -Np1 -i ../readline-6.2-fixes-1.patch 
-./configure --prefix=/usr --libdir=/usr
-make SHLIB_LIBS=-lncurses
-make install
-mv -v /lib/lib{readline,history}.a /usr/lib
-mv -v /usr/lib{readline,history}.a /usr/lib
 ./configure --prefix=/usr --libdir=/lib
 make SHLIB_LIBS=-lncurses
 make install
@@ -627,7 +638,9 @@ rm -rf readline-6.2
 tar xf bash-4.2.tar.gz 
 cd bash-4.2
 patch -Np1 -i ../bash-4.2-fixes-4.patch 
-./configure --prefix=/usr --bindir=/bin --htmldir=/usr/share/doc/bash-4.2 --without-bash-malloc --with-installed-readline
+./configure --prefix=/usr --bindir=/bin \
+	--htmldir=/usr/share/doc/bash-4.2 --without-bash-malloc \
+	--with-installed-readline
 make
 make install
 exec /bin/bash --login +h
@@ -669,7 +682,10 @@ rm -rf gdbm-1.10
 
 tar xf inetutils-1.9.1.tar.gz 
 cd inetutils-1.9.1
-./configure --prefix=/usr --libexecdir=/usr/sbin --localstatedir=/var --disable-ifconfig --disable-logger --disable-syslogd --disable-whois --disable-servers
+./configure --prefix=/usr --libexecdir=/usr/sbin \
+	--localstatedir=/var --disable-ifconfig \
+	--disable-logger --disable-syslogd \
+	--disable-whois --disable-servers
 make
 make check
 make install
@@ -681,18 +697,33 @@ cd ..
 rm -rf inetutils-1.9.1
 
 
+###############
+# Perl 5.14.2 #
+###############
+
 tar xf perl-5.14.2.tar.bz2 
 cd perl-5.14.2
 patch -Np1 -i ../perl-5.14.2-security_fix-1.patch 
 echo "127.0.0.1 localhost $(hostname)" > /etc/hosts
-sed -i -e "s|BUILD_ZLIB\s*= True|BUILD_ZLIB = False|"                  -e "s|INCLUDE\s*= ./zlib-src|INCLUDE    = /usr/include|"        -e "s|LIB\s*= ./zlib-src|LIB        = /usr/lib|"             cpan/Compress-Raw-Zlib/config.in
-sh Configure -des -Dprefix=/usr                   -Dvendorprefix=/usr                             -Dman1dir=/usr/share/man/man1                   -Dman3dir=/usr/share/man/man3                   -Dpager="/usr/bin/less -isR"                    -Duseshrplib
+sed -i -e "s|BUILD_ZLIB\s*= True|BUILD_ZLIB = False|" \
+	-e "s|INCLUDE\s*= ./zlib-src|INCLUDE = /usr/include|" \
+	-e "s|LIB\s*= ./zlib-src|LIB = /usr/lib|" cpan/Compress-Raw-Zlib/config.in
+sh Configure -des -Dprefix=/usr \
+	-Dvendorprefix=/usr \
+	-Dman1dir=/usr/share/man/man1 \
+	-Dman3dir=/usr/share/man/man3 \
+	-Dpager="/usr/bin/less -isR" \
+	-Duseshrplib
 make
 make test
 make install
 cd ..
 rm -rf perl-5.14.2
 
+
+#################
+# Autoconf 2.68 #
+#################
 
 tar xf autoconf-2.68.tar.bz2 
 cd autoconf-2.68
@@ -704,17 +735,24 @@ cd ..
 rm -rf autoconf-2.68
 
 
+###################
+# Automake 1.11.3 #
+###################
+
 tar xf automake-1.11.3.tar.xz 
 cd automake-1.11.3
 ./configure --prefix=/usr --docdir=/usr/share/doc/automake-1.11.3
 make
 make check
 cat tests/test-suite.log  | grep FAIL
-cat test-suite.log | grep FAIL
 make install
 cd ..
-rm -rf automake-1.11.3 autoconf-2.68
+rm -rf automake-1.11.3
 
+
+#################
+# Diffutils 3.2 #
+##############$$$
 
 tar xf diffutils-3.2.tar.gz 
 cd diffutils-3.2
@@ -725,6 +763,10 @@ make install
 cd ..
 rm -rf diffutils-3.2
 
+
+##############
+# Gawk 4.0.0 #
+##############
 
 tar xf gawk-4.0.0.tar.bz2 
 cd gawk-4.0.0
@@ -738,6 +780,10 @@ cd ..
 rm -rf gawk-4.0.0
 
 
+###################
+# Findutils 4.4.2 #
+###################
+
 tar xf findutils-4.4.2.tar.gz 
 cd findutils-4.4.2
 ./configure --prefix=/usr --libexecdir=/usr/lib/findutils --localstatedir=/var/lib/locate
@@ -749,6 +795,10 @@ sed -i 's/find:=${BINDIR}/find:=\/bin/' /usr/bin/updatedb
 cd ..
 rm -rf findutils-4.4.2
 
+
+###############
+# Flex 2.5.35 #
+###############
 
 tar xf flex-2.5.35.tar.bz2 
 cd flex-2.5.35
@@ -775,6 +825,10 @@ cd ..
 rm -rf flex-2.5.35
 
 
+####################
+# Gettext 0.18.1.1 #
+####################
+
 tar xf gettext-0.18.1.1.tar.gz 
 cd gettext-0.18.1.1
 ./configure --prefix=/usr --docdir=/usr/share/doc/gettext-0.18.1.1
@@ -784,6 +838,10 @@ make install
 cd ..
 rm -rf gettext-0.18.1.1
 
+
+##############
+# Groff 1.21 #
+##############
 
 tar xf groff-1.21.tar.gz 
 cd groff-1.21
@@ -796,6 +854,10 @@ cd ..
 rm -rf groff-1.21
 
 
+############
+# Xz 5.0.3 #
+############
+
 tar xf xz-5.0.3.tar.bz2 
 cd xz-5.0.3
 ./configure --prefix=/usr --libdir=/lib --docdir=/usr/share/doc/xz-5.0.3
@@ -804,6 +866,12 @@ make check
 make pkgconfigdir=/usr/lib/pkgconfig install
 cd ..
 rm -rf xz-5.0.3
+
+
+#############
+# Grub 1.99 #
+#############
+
 tar xf grub-1.99.tar.gz 
 cd grub-1.99
 ./configure --prefix=/usr --sysconfdir=/etc --disable-grub-emu-usb --disable-efiemu --disable-werror
@@ -813,16 +881,9 @@ cd ..
 rm -rf grub-1.99
 
 
-tar xf iproute2-3.2.0.tar.xz 
-cd iproute2-3.2.0
-./configure --prefix=/usr --bindir=/bin
-make
-./configure --prefix=/usr --bindir=/bin
-make
-make check
-cd ..
-rm -rf iproute2-3.2.0
-
+############
+# Gzip 1.4 #
+############
 
 tar xf gzip-1.4.tar.gz 
 cd gzip-1.4
@@ -836,6 +897,10 @@ cd ..
 rm -rf gzip-1.4
 
 
+#################
+# IProute 3.2.0 #
+#################
+
 tar xf iproute2-3.2.0.tar.xz 
 cd iproute2-3.2.0
 sed -i '/^TARGETS/s@arpd@@g' misc/Makefile 
@@ -847,6 +912,10 @@ make DESTDIR= MANDIR=/usr/share/man DOCDIR=/usr/share/doc/iproute2-3.2.0 install
 cd ..
 rm -rf iproute2-3.2.0
 
+
+##############
+# Kbd 1.15.2 #
+##############
 
 tar xf kbd-1.15.2.tar.gz 
 cd kbd-1.15.2
@@ -861,17 +930,29 @@ cd ..
 rm -rf kbd-1.15.2
 
 
+##########
+# Kmod 5 #
+##########
+
 tar xf kmod-5.tar.xz 
 cd kmod-5
 liblzma_CFLAGS="-I/usr/include" liblzma_LIBS="-L/lib -llzma" zlib_CFLAGS="-I/usr/include" zlib_LIBS="-L/lib -lz" ./configure --prefix=/usr --bindir=/bin --libdir=/lib --sysconfdir=/etc --with-xz --with-zlib
 make
 make check
 make pkgconfigdir=/usr/lib/pkgconfig install
-for target in depmod insmod modinfo modprobe rmmod; do   ln -sv ../bin/kmod /sbin/$target; done
+
+for target in depmod insmod modinfo modprobe rmmod; do
+	ln -sv ../bin/kmod /sbin/$target
+done
+
 ln -sv kmod /bin/lsmod
 cd ..
 rm -rf kmod-5
 
+
+#############
+# Less 4.44 #
+#############
 
 tar xf less-444.tar.gz 
 cd less-444
@@ -881,6 +962,10 @@ make install
 cd ..
 rm -rf less-444
 
+
+#####################
+# Libpipeline 1.2.0 #
+#####################
 
 tar xf libpipeline-1.2.0.tar.gz 
 cd libpipeline-1.2.0
@@ -892,6 +977,10 @@ cd ..
 rm -rf libpipeline-1.2.0
 
 
+#############
+# Make 3.82 #
+#############
+
 tar xf make-3.82.tar.bz2 
 cd make-3.82
 ./configure --prefix=/usr
@@ -902,15 +991,26 @@ cd ..
 rm -rf make-3.82
 
 
+################
+# Man DB 2.6.1 #
+################
+
 tar xf man-db-2.6.1.tar.gz 
 cd man-db-2.6.1
-PKG_CONFIG=/tools/bin/true libpipeline_CFLAGS='' libpipeline_LIBS='-lpipeline' ./configure --prefix=/usr --libexecdir=/usr/lib --docdir=/usr/share/doc/man-db-2.6.1 --sysconfdir=/etc --disable-setuid --with-browser=/usr/bin/lynx --with-vgrind=/usr/bin/vgrind --with-grap=/usr/bin/grap
+PKG_CONFIG=/tools/bin/true libpipeline_CFLAGS='' libpipeline_LIBS='-lpipeline' \
+	./configure --prefix=/usr --libexecdir=/usr/lib --docdir=/usr/share/doc/man-db-2.6.1 \
+	--sysconfdir=/etc --disable-setuid --with-browser=/usr/bin/lynx \
+	--with-vgrind=/usr/bin/vgrind --with-grap=/usr/bin/grap
 make
 make check
 make install
 cd ..
 rm -rf man-db-2.6.1
 
+
+###############
+# Patch 2.6.1 #
+###############
 
 tar xf patch-2.6.1.tar.bz2 
 cd patch-2.6.1
@@ -923,12 +1023,17 @@ cd ..
 rm -rf patch-2.6.1
 
 
+################
+# Shadow 4.1.5 #
+################
+
 tar xf shadow-4.1.5.tar.bz2 
 cd shadow-4.1.5
 patch -Np1 -i ../shadow-4.1.5-nscd-1.patch 
 sed -i 's/groups$(EXEEXT) //' src/Makefile.in 
 find man -name Makefile.in -exec sed -i 's/groups\.1 / /' {} \;
-sed -i -e 's@#ENCRYPT_METHOD DES@ENCRYPT_METHOD SHA512@' -e 's@/var/spool/mail@/var/mail@' etc/login.defs 
+sed -i -e 's@#ENCRYPT_METHOD DES@ENCRYPT_METHOD SHA512@' \
+	-e 's@/var/spool/mail@/var/mail@' etc/login.defs 
 ./configure --sysconfdir=/etc
 make
 make install
@@ -941,6 +1046,10 @@ passwd root
 cd ..
 rm -rf shadow-4.1.5
 
+
+################
+# Sysklogd 1.5 #
+################
 
 tar xf sysklogd-1.5.tar.gz 
 cd sysklogd-1.5
@@ -962,15 +1071,24 @@ cd ..
 rm -rf sysklogd-1.5
 
 
+####################
+# SysVinit 2.88dsf #
+####################
+
 tar xf sysvinit-2.88dsf.tar.bz2 
 cd sysvinit-2.88dsf
 sed -i 's@Sending processes@& configured via /etc/inittab@g' src/init.c 
-sed -i -e 's/utmpdump wall/utmpdump/' -e '/= mountpoint/d' -e 's/mountpoint.1 wall.1//' src/Makefile 
+sed -i -e 's/utmpdump wall/utmpdump/' \
+	-e '/= mountpoint/d' -e 's/mountpoint.1 wall.1//' src/Makefile 
 make -C src
 make -C src install
 cd ..
 rm -rf sysvinit-2.88dsf
 
+
+############
+# Tar 1.26 #
+############
 
 tar xf tar-1.26.tar.bz2 
 cd tar-1.26
@@ -982,6 +1100,10 @@ make -C doc install-html docdir=/usr/share/doc/tar-1.2.6
 cd ..
 rm -rf tar-1.26
 
+
+##################
+# Textinfo 4.13a #
+##################
 
 tar xf texinfo-4.13a.tar.gz 
 cd texinfo-4.13
@@ -1003,12 +1125,21 @@ cd ..
 rm -rf texinfo-4.13
 
 
+##############
+# Udev 1.8.1 #
+##############
+
 tar xf udev-181.tar.xz 
 cd udev-181
 tar xf ../udev-config-20100128.tar.bz2 
 install -dv /lib/{firmware,udev/device/pts}
 mknod -m0666 /lib/udev/device/null c 1 3
-BLKID_CFLAGS="-I/usr/include/blkid" BLKID_LIBS="-L/lib -lblkid" KMOD_CFLAGS="-I/usr/include" KMOD_LIBS="-L/lib -lkmod" ./configure --prefix=/usr --with-rootprefixe='' --bindir=/sbin --sysconfdir=/etc --libexecdir=/lib --enable-rule-generator --disable-introspection --disable-keymap --disable-gudev --with-usb-ids-path=no --with-pci-ids-path=no --with-systemdsystemunitdir=no
+BLKID_CFLAGS="-I/usr/include/blkid" BLKID_LIBS="-L/lib -lblkid" \
+	KMOD_CFLAGS="-I/usr/include" KMOD_LIBS="-L/lib -lkmod" ./configure \
+	--prefix=/usr --with-rootprefixe='' --bindir=/sbin --sysconfdir=/etc \
+	--libexecdir=/lib --enable-rule-generator --disable-introspection \
+	--disable-keymap --disable-gudev --with-usb-ids-path=no \
+	--with-pci-ids-path=no --with-systemdsystemunitdir=no
 make
 make check
 make install
@@ -1020,6 +1151,10 @@ cd ../../
 rm -rf udev-181
 
 
+###########
+# Vim 7.3 #
+###########
+
 tar xf vim-7.3.tar.bz2 
 cd vim73/
 echo '#define SYS_VIMRC_FILE "/etc/vimrc"' >> src/feature.h 
@@ -1027,7 +1162,11 @@ echo '#define SYS_VIMRC_FILE "/etc/vimrc"' >> src/feature.h
 make test
 make install
 ln -sv vim /usr/bin/vi
-for L in  /usr/share/man/{,*/}man1/vim.1; do     ln -sv vim.1 $(dirname $L)/vi.1; done
+
+for L in  /usr/share/man/{,*/}man1/vim.1; do
+	ln -sv vim.1 $(dirname $L)/vi.1
+done
+
 ln -sv ../vim/vim73/doc /usr/share/doc/vim-7.3
 
 cat > /etc/vimrc << "EOF"
@@ -1046,6 +1185,8 @@ EOF
 cd ..
 rm -rf vim73/
 
+
+# Done with /tools directory #
+
 cd /
 rm -rf /tools
-
